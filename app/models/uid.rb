654d8914d270afafcef493d8559251a49b436c47
Sue White <sue@mailworks.org>
2014-04-24 04:31:37 +0000

    remove useless method

--- app/models/uid.rb
+++ app/models/uid.rb
@@ -1,10 +1,6 @@
 class Uid < ActiveRecord::Base
   self.primary_key = 'id'
 
-  def initialize(id)
-    self.id = id
-  end
-
   def signed
     Uid.connection.select_all("select * from sigs where signer = '#{self.id}'").map{|h| h['signed']}
   end
